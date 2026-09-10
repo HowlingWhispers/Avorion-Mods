736daf6f0627ed33cca70a0c0199e295b3409f0f
HowlingWhispers <howlingwhispers@proton.me>
2026-09-10 21:13:15 +0200

    Restructure R-Utility as installable Avorion mod

---
+++ R-Utility-Laser/modinfo.lua
@@ -0,0 +1,17 @@
+meta =
+{
+    id = "howlingwhispers.rutilitylaser",
+    name = "Howling Whispers - R-Utility Laser",
+    title = "R-Utility Laser",
+    description = "A rebuilt hybrid resource turret that mines stone and salvages metal. New players receive four Iron starter turrets for testing.",
+    version = "0.1.0-dev",
+    author = "Howling Whispers",
+
+    dependencies = {
+        {id = "Avorion", min = "2.0"}
+    },
+
+    serverSideOnly = false,
+    clientSideOnly = false,
+    saveGameAltering = true,
+}
